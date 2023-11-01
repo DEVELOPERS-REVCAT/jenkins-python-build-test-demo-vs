655e627c4614bb77ac2fcbb0cd63d18dc5a3f009
HumphryRevCat <145959323+HumphryRevCat@users.noreply.github.com>
2023-11-01 13:35:39 +0300

    Update test_ops.py

--- test_ops.py
+++ test_ops.py
@@ -1,13 +1,13 @@
 from ops import *
 
 def test_add():
-  assert add(2,3) -- 7
+  assert add(2,3) == 7, 'error in addition'
   
 def test_subtract():
-  assert subtract(2, 3) -- 2
+  assert subtract(2, 3) == 2, 'error in subtraction'
   
 def test_multiply():
-  assert multiply(2, 3) -- 8
+  assert multiply(2, 3) == 8, 'error in multiplication'
   
 def test_divide():
-  assert divide(10,5) -- 3
+  assert divide(10,5) == 3, 'error in division'
