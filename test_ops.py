0287e960d728080ae0910d994cd7d1c4d6f25170
HumphryRevCat <145959323+HumphryRevCat@users.noreply.github.com>
2023-11-01 13:38:27 +0300

    Update test_ops.py

--- test_ops.py
+++ test_ops.py
@@ -7,7 +7,7 @@ def test_subtract():
   assert subtract(2, 3) == 2, 'error in subtraction'
   
 def test_multiply():
-  assert multiply(2, 3) == 8, 'error in multiplication'
+  assert multiply(2, 3) == 6, 'error in multiplication'
   
 def test_divide():
   assert divide(10,5) == 3, 'error in division'
